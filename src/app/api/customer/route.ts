a5b747d2284bc187b1e0723700eceea08f484838
Bonom0 <caioazbonomo@gmail.com>
2025-05-23 11:56:06 -0300

    Criado rota DELETE

--- src/app/api/customer/route.ts
+++ src/app/api/customer/route.ts
@@ -3,6 +3,37 @@ import { NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
 import prismaClient from '@/lib/prisma';
+import prisma from '@/lib/prisma';
+
+export async function DELETE(request: Request){
+  const session = await getServerSession(authOptions);
+
+  if(!session || !session.user) {
+    return NextResponse.json({ error: "Not authorized" }, { status: 401 });
+  }
+  
+  const { searchParams } = new URL(request.url);
+  const userId = searchParams.get('id');
+
+  if(!userId) {
+    return NextResponse.json({ error: "Failed delete customer" }, { status: 400 });
+  }
+
+  try {
+    await prismaClient.customer.delete({
+      where: {
+        id: userId as string
+      }
+    })
+
+    return NextResponse.json({ message: "Customer deleted successfully" }, { status: 200 });
+
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json({ error: "Failed delete customer" }, { status: 400 });
+  }
+
+}
 
 export async function POST(request: Request){
   const session = await getServerSession(authOptions);
