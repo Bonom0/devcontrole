6309e8e01be55952c9ad990cc79b4c9c8dc08507
Bonom0 <caioazbonomo@gmail.com>
2025-05-26 10:31:57 -0300

    Adicionado renderização condicional para exibir mensagem quando não tiver tickets cadastrados.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -56,6 +56,12 @@ export default async function Dashboard() {
             ))}
           </tbody>
         </table>
+
+        {tickets.length === 0 && (
+          <h1 className="px-2 md:px-2 text-gray-600">
+            Nenhum ticket aberto foi encontrado...
+          </h1>
+        )}
       </main>
     </Container>
   );
