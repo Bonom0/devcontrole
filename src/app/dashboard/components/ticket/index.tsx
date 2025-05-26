37177787ee1fe185ec93b6ba55a01bade2a201c0
Bonom0 <caioazbonomo@gmail.com>
2025-05-26 10:31:12 -0300

    Alterado para use client. Importado api e useRouter. Adicionado router.refresh após atualizar o status.

--- src/app/dashboard/components/ticket/index.tsx
+++ src/app/dashboard/components/ticket/index.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import { CustomerProps } from "@/utils/customer.type";
 import { TicketProps } from "@/utils/ticket.type";
-import { FiTrash2, FiFile } from "react-icons/fi";
+import { FiFile, FiCheckSquare } from "react-icons/fi";
+import { api } from "@/lib/api";
+import { useRouter } from "next/navigation";
 
 interface TicketItemProps {
   ticket: TicketProps;
@@ -8,6 +12,20 @@ interface TicketItemProps {
 }
 
 export function TicketItem({ customer, ticket }: TicketItemProps) {
+  const router = useRouter();
+
+  async function handleChangeStatus() {
+    try {
+      const response = await api.patch("api/ticket", {
+        id: ticket.id,
+      });
+
+      router.refresh();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <>
       <tr className="border-b-2 border-b-slate-200 h-16 last:border-b-0 bg-slate-100 hover:bg-gray-200 duration-300">
@@ -24,8 +42,12 @@ export function TicketItem({ customer, ticket }: TicketItemProps) {
         </td>
 
         <td className="text-left">
-          <button className="mr-2">
-            <FiTrash2 size={24} color="#EF4444" />
+          <button className="mr-3">
+            <FiCheckSquare
+              size={24}
+              color="#131313"
+              onClick={handleChangeStatus}
+            />
           </button>
           <button>
             <FiFile size={24} color="#3b82f6" />
