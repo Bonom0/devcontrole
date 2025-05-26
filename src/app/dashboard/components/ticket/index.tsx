4b8f2ec550a6132788b2ba851c46929a470a8e68
Bonom0 <caioazbonomo@gmail.com>
2025-05-26 09:38:26 -0300

    Importado interfaces. Criado interface TicketItemProps. Alterado dados de exemplo para dados reais do banco.

--- src/app/dashboard/components/ticket/index.tsx
+++ src/app/dashboard/components/ticket/index.tsx
@@ -1,15 +1,26 @@
+import { CustomerProps } from "@/utils/customer.type";
+import { TicketProps } from "@/utils/ticket.type";
 import { FiTrash2, FiFile } from "react-icons/fi";
 
-export function TicketItem() {
+interface TicketItemProps {
+  ticket: TicketProps;
+  customer: CustomerProps | null;
+}
+
+export function TicketItem({ customer, ticket }: TicketItemProps) {
   return (
     <>
       <tr className="border-b-2 border-b-slate-200 h-16 last:border-b-0 bg-slate-100 hover:bg-gray-200 duration-300">
-        <td className="text-left pl-1">Mercado Silva</td>
-
-        <td className="text-left">01/04/2024</td>
+        <td className="text-left pl-1">{customer?.name}</td>
 
         <td className="text-left">
-          <span className="bg-green-500 px-2 py-1 rounded">ABERTO</span>
+          {ticket.created_at?.toLocaleDateString("pt-br")}
+        </td>
+
+        <td className="text-left">
+          <span className="bg-green-500 px-2 py-1 rounded">
+            {ticket.status}
+          </span>
         </td>
 
         <td className="text-left">
